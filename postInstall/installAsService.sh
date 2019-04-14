e090648f777bdc80f7e100e29955f2a5c463d1d2
John Rucker <JHRucker@outlook.com>
2019-04-14 17:19:08 -0500

    ed

--- postInstall/installAsService.sh
+++ postInstall/installAsService.sh
@@ -2,10 +2,10 @@
 # From DOS prompt type (git update-index --chmod=+x installAsService.sh) to make this file executable.
 set -e
 echo "NPM post install shell that installs this app as service starts now..."
-echo "Set irdclient as defalut group for weatherGauge -> sudo chown :irdclient ../weatherGauge"
-sudo chown :irdclient ../weatherGauge
-echo "Give default group write access to the weatherGauge directory -> sudo chmod g+w ../weatherGauge"
-sudo chmod g+w ../weatherGauge
+#echo "Set irdclient as defalut group for weatherGauge -> sudo chown :irdclient ../weatherGauge"
+#sudo chown :irdclient ../weatherGauge
+#echo "Give default group write access to the weatherGauge directory -> sudo chmod g+w ../weatherGauge"
+#sudo chmod g+w ../weatherGauge
 echo "Install D-Bus config file for this service -> sudo cp ./postInstall/dbus.conf /etc/dbus-1/system.d/weatherGauge.conf"
 sudo cp ./postInstall/dbus.conf /etc/dbus-1/system.d/weatherGauge.conf
 echo "Install systemd service file -> sudo cp -n ./postInstall/server.service /etc/systemd/system/weatherGauge.service"
--- postInstall/un-installAsService.sh
+++ postInstall/un-installAsService.sh
@@ -3,12 +3,12 @@
 # From DOS prompt type (git update-index --chmod=+x un-installAsService.sh) to make this file executable.
 # set -e  <-This will cause script to end on first error with error code
 echo "un-install service will stop and remove this app as a service starts now..."
-#echo "Stop service -> systemctl stop sbPowerGauge.service"
-#sudo systemctl stop sbPowerGauge.service
-echo "Disable servers so it will not start on reboot ->systemctl disable sbPowerGauge.service"
-sudo systemctl disable sbPowerGauge.service
-echo "Remove the systemd service file -> rm /etc/systemd/system/sbPowerGauge.service"
-sudo rm /etc/systemd/system/sbPowerGauge.service
-echo "Remove D-Bus config file sudo rm /etc/dbus-1/system.d/sbPowerGauge.conf"
-sudo rm /etc/dbus-1/system.d/sbPowerGauge.conf
+#echo "Stop service -> systemctl stop weatherGauge.service"
+#sudo systemctl stop weatherGauge.service
+echo "Disable servers so it will not start on reboot ->systemctl disable weatherGauge.service"
+sudo systemctl disable weatherGauge.service
+echo "Remove the systemd service file -> rm /etc/systemd/system/weatherGauge.service"
+sudo rm /etc/systemd/system/weatherGauge.service
+echo "Remove D-Bus config file sudo rm /etc/dbus-1/system.d/weatherGauge.conf"
+sudo rm /etc/dbus-1/system.d/weatherGauge.conf
 echo "un-instal shell is complete. Reboot server to stop service."
