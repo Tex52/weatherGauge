63d4d20b04fd321a1b47c1c69898e3b0f9a19e84
John Rucker <JHRucker@outlook.com>
2019-04-16 06:48:30 -0500

    ed

--- thisAppsCfgManager.js
+++ thisAppsCfgManager.js
@@ -36,7 +36,7 @@ class appManager extends EventEmitter{
         this.status = 'ipl, ' + (new Date()).toLocaleTimeString() + ', ' + (new Date()).toLocaleDateString();
         this.value = 'Not Set Yet';
         this._okToSend = true;
-        this.bPrl = new BLEperipheral(this.config.dBusName, this.config.uuid, this._bleConfig, false);
+        this.bPrl = new BLEperipheral(this.config.dBusName, this.config.uuid, self._bleConfig, false);
         self = this;  
 
         this.bPrl.on('ConnectionChange', (connected)=>{
